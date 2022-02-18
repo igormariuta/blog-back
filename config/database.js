841f09d5c1bd9fe026071c6090dfc35ab4d98b81
Igor Mariuta <igor.mariuta@amdaris.com>
2022-02-18 17:27:52 +0200

    m ?

--- config/database.js
+++ config/database.js
@@ -1,11 +1,44 @@
-const path = require('path');
+// const path = require('path');
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "back"),
+//       user: env("DATABASE_USERNAME", ""),
+//       password: env("DATABASE_PASSWORD", ""),
+//       ssl: env.bool("DATABASE_SSL", false),
+//     },
+//   },
+// });
+
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });

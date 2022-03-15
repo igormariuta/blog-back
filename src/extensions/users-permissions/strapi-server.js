94d2ff791ed601a881aac59b6227b16a2799d46d
Igor Mariuta <igor.mariuta@amdaris.com>
2022-03-16 00:06:02 +0200

    post custom control / user custom extension

---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,33 @@
+const { sanitize } = require("@strapi/utils");
+
+const sanitizeOutput = (user, ctx) => {
+  const schema = strapi.getModel("plugin::users-permissions.user");
+  const { auth } = ctx.state;
+  return sanitize.contentAPI.output(user, schema, { auth });
+};
+
+module.exports = (plugin) => {
+  plugin.controllers.user.findOne = (ctx) => {
+    const { id: username } = ctx.params;
+
+    const response = strapi.db
+      .query("plugin::users-permissions.user")
+      .findOne({
+        where: { username: { $eq: username } },
+        populate: [
+          "posts.previewImage",
+          "comments",
+          "followers.avatar",
+          "following.avatar",
+          "avatar",
+        ],
+      })
+      .then((res) => {
+        return sanitizeOutput(res, ctx);
+      });
+
+    return response;
+  };
+
+  return plugin;
+};
